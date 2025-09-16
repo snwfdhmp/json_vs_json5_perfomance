6c3120654b915843cf517291413f661c3b0d49cf
snwfdhmp <snwfdhmp@gmail.com>
2025-09-17 01:30:19 +0200

    minor improvements

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-'use strict';
+"use strict";
 
-const Benchmark = require('benchmark');
-const JSON5 = require('json5');
+const Benchmark = require("benchmark");
+const JSON5 = require("json5");
 
-const sample = require('fs').readFileSync(__dirname + '/examples/project-stub.json', 'utf8');
-const parseSample = JSON.stringify(sample);
-const stringifySample = require('node-eval')(`module.exports = ${sample};`);
+const sample = require("fs").readFileSync(
+  __dirname + "/examples/project-stub.json",
+  "utf8"
+);
+const parseSample = sample;
+const stringifySample = require("node-eval")(`module.exports = ${sample};`);
 
-const suite = new Benchmark.Suite;
+const suite = new Benchmark.Suite();
 const parsers = {
-    JSON5: JSON5.parse,
-    JSON: JSON.parse
+  JSON5: JSON5.parse,
+  JSON: JSON.parse,
 };
 const stringifiers = {
-    JSON5: JSON5.stringify,
-    JSON: JSON.stringify
+  JSON5: JSON5.stringify,
+  JSON: JSON.stringify,
 };
 
 function addSuites(suite, functions, fnName, sample) {
-    for (let name in functions) {
-        suite.add(`${name}#${fnName}`, () => functions[name](sample));
-    }
+  for (let name in functions) {
+    suite.add(`${name}#${fnName}`.padEnd(16), () => functions[name](sample));
+  }
 }
 
-addSuites(suite, parsers, 'parse', parseSample);
-addSuites(suite, stringifiers, 'stringify', stringifySample);
+addSuites(suite, parsers, "parse", parseSample);
+addSuites(suite, stringifiers, "stringify", stringifySample);
 
-suite
-    .on('cycle', event => console.log(String(event.target)))
-    .run();
+suite.on("cycle", (event) => console.log(String(event.target))).run();
